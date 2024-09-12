9c7003d5d848d2504797b75d69a462914deeaf69
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2024-09-12 15:57:40 +0300

    feat: add hash-object command

---
+++ cmd/hash_object.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(hashObjectCmd)
+}
+
+func hash_data(data []byte) string {
+	hash := sha1.Sum(data)
+	return hex.EncodeToString(hash[:])
+}
+
+var hashObjectCmd = &cobra.Command{
+	Use:   "hash-object",
+	Short: "hash file into object",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			log.Fatal("file args is required")
+		}
+		file, err := os.Open(args[0])
+		if err != nil {
+			log.Fatal("err: ", err)
+		}
+		defer file.Close()
+		data, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		oid := hash_data(data)
+		outFile, err := os.Create(GIT_DIR + "/objects/" + oid)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer outFile.Close()
+		_, err = outFile.Write(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
--- cmd/init.go
+++ cmd/init.go
@@ -2,9 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+const GIT_DIR = ".lgit"
 
 func init() {
 	rootCmd.AddCommand(initCmd)
@@ -14,6 +17,16 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "init git repo",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Initing repo")
+		currentPath, _ := os.Getwd()
+		fmt.Printf("Initing empty repo in %s/%s \n", currentPath, GIT_DIR)
+		err := os.Mkdir(GIT_DIR, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+		err = os.Mkdir(GIT_DIR+"/objects", os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+
 	},
 }
